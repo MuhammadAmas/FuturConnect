343f8584ca12543a44cda84ae03b9ee85baff7fd
muhammadamas <amaswaseem@gmail.com>
2024-10-18 07:02:41 +0500

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -54,108 +54,106 @@ function App() {
         }
       />
 
-      {/* Dashboard and Other Routes with DefaultLayout */}
-      <Route element={<DefaultLayout />}>
-        <Route
-          path="/dashboard"
-          element={
-            <>
-              <PageTitle title="Dashboard | Futur Connect" />
-              <ECommerce />
-            </>
-          }
-        />
-        <Route
-          path="/room-management"
-          element={
-            <>
-              <PageTitle title="Room Management | Futur Connect" />
-              <RoomManagement />
-            </>
-          }
-        />
-        <Route
-          path="/calendar"
-          element={
-            <>
-              <PageTitle title="Calendar | Futur Connect" />
-              <Calendar />
-            </>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <>
-              <PageTitle title="Profile | Futur Connect" />
-              <Profile />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-elements"
-          element={
-            <>
-              <PageTitle title="Form Elements | Futur Connect" />
-              <FormElements />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-layout"
-          element={
-            <>
-              <PageTitle title="Form Layout | Futur Connect" />
-              <FormLayout />
-            </>
-          }
-        />
-        <Route
-          path="/tables"
-          element={
-            <>
-              <PageTitle title="Tables | Futur Connect" />
-              <Tables />
-            </>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <>
-              <PageTitle title="Settings | Futur Connect" />
-              <Settings />
-            </>
-          }
-        />
-        <Route
-          path="/chart"
-          element={
-            <>
-              <PageTitle title="Basic Chart | Futur Connect" />
-              <Chart />
-            </>
-          }
-        />
-        <Route
-          path="/ui/alerts"
-          element={
-            <>
-              <PageTitle title="Alerts | Futur Connect" />
-              <Alerts />
-            </>
-          }
-        />
-        <Route
-          path="/ui/buttons"
-          element={
-            <>
-              <PageTitle title="Buttons | Futur Connect" />
-              <Buttons />
-            </>
-          }
-        />
-      </Route>
+      {/* Main app routes using DefaultLayout */}
+      <Route
+        path="/dashboard"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Dashboard | Futur Connect" />
+            <ECommerce />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/room-management"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Room Management | Futur Connect" />
+            <RoomManagement />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/calendar"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Calendar | Futur Connect" />
+            <Calendar />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Profile | Futur Connect" />
+            <Profile />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/forms/form-elements"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Form Elements | Futur Connect" />
+            <FormElements />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/forms/form-layout"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Form Layout | Futur Connect" />
+            <FormLayout />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/tables"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Tables | Futur Connect" />
+            <Tables />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Settings | Futur Connect" />
+            <Settings />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/chart"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Basic Chart | Futur Connect" />
+            <Chart />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/ui/alerts"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Alerts | Futur Connect" />
+            <Alerts />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/ui/buttons"
+        element={
+          <DefaultLayout>
+            <PageTitle title="Buttons | Futur Connect" />
+            <Buttons />
+          </DefaultLayout>
+        }
+      />
     </Routes>
   );
 }
